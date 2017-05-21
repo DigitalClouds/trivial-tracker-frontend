7df0a51a035f6201fdb3199a04462454f1b0f1f9
Zachary Markham <zmarkham@outlook.com>
2017-05-21 08:04:36 +0100

    Added login route

--- src/main.js
+++ src/main.js
@@ -2,16 +2,18 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Question from './Question.vue'
 import Location from './Location.vue'
+import Login from './Login.vue'
 
 const NotFound = { template: '<p>Page not found</p>' };
 const routes = [
-    { path: '/', component: Question},
+    { path: '/', component: Login},
+    { path: '/question', component: Question},
     { path: '/location', name: 'location', component: Location, props: true}
 ];
 
 Vue.use(VueRouter);
 
-const router = new VueRouter({
+export const router = new VueRouter({
     routes
 });
 
